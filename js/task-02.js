db5932f7d8adfc9a196ee75bb30e28e461a3c17b
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-11-14 12:34:52 +0200

    finish

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,24 @@ const ingredients = [
   "Condiments",
 ];
 
-const items = ingredients.forEach((ingredient) => {
-  const item = document.createElement("li");
-  item.textContent = ingredient;
-  item.classList.add("item");
-  console.log(item);
-  const list = document.querySelector("#ingredients");
-  list.append(item);
-});
+// const items = ingredients.forEach((ingredient) => {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.classList.add("item");
+//   console.log(item);
+//   const list = document.querySelector("#ingredients");
+//   list.append(item);
+// });
+
+const ingredientList = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.classList.add("item");
+    itemEl.textContent = ingredient;
+    return itemEl;
+  });
+};
+
+const elements = ingredientList(ingredients);
+const listEl = document.querySelector("#ingredients");
+listEl.append(...elements);
--- js/task-08.js
+++ js/task-08.js
@@ -4,10 +4,22 @@ function onFormSubmit(event) {
   event.preventDefault();
   const newForm = new FormData(event.currentTarget);
   console.log(newForm);
+
+  const email = event.currentTarget.elements.email.value;
+
+  const password = event.currentTarget.elements.password.value;
+
+  if (email.length === 0 || password.length === 0) {
+    alert("Заповніть всі поля");
+    return;
+  }
+
   newForm.forEach((name, value) => {
     console.log(value);
     console.log(name);
   });
+
+  form.reset();
 }
 //   const formElements = event.currentTarget.elements;
 //   const mail = formElements.email.value;
